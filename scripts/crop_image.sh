5f0f764f7ca814ba28d274c08581fccbf9abd7b5
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-01-14 15:42:08 +0000

    Removed reduntant scripts.

--- scripts/crop_image.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-mkdir -p data/crop
-
-convert -crop 2100x2500+7300+2400 data/raw/TileScan_001_Merging001_ch00.jpg data/crop/TileScan_001_Merging001_ch00.jpg
-convert -crop 1000x1000+8300+3400 data/raw/TileScan_001_Merging001_ch00.jpg data/crop/small.jpg
-convert -crop 1000x1000+8300+3400 data/raw/TileScan_001_Merging001_ch00.jpg data/crop/small.tiff
