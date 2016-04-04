7cc59d54597450c5bf5319225821319e59814349
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2016-04-04 16:03:51 +0100

    Updated version number

--- scripts/annotate.py
+++ scripts/annotate.py
@@ -29,7 +29,7 @@ from jicbioimage.segment import connected_components, watershed_with_seeds
 from jicbioimage.illustrate import AnnotatedImage
 
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 # Suppress spurious scikit-image warnings.
 # warnings.filterwarnings("ignore", module="skimage.exposure._adapthist")
